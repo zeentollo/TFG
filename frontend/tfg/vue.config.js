f952671c773b8c9b19cf04872bbf37c9724d6581
unknown <danielmartiguerrero@gmail.com>
2024-05-29 01:13:21 +0200

    Configurando render

--- frontend/tfg/vue.config.js
+++ frontend/tfg/vue.config.js
@@ -1,6 +1,4 @@
-module.exports = {
-  devServer: {
-    allowedHosts: 'all'
-  }
-};
-
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true
+})
